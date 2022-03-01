a5523816e81f68cf620afb84761bc35a31cbc170
Dhiraj Subedi <dhirajsubedi924@gmail.com>
2022-03-01 16:42:49 +0545

    json dat

--- lib/first.dart
+++ lib/first.dart
@@ -1,8 +1,13 @@
 
 
+// import 'dart:ffi';
+// import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:projects/drawer.dart';
-import 'package:projects/name_card_widget.dart';
+// import 'package:projects/name_card_widget.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class First extends StatefulWidget {
   const First({ Key? key }) : super(key: key);
@@ -12,29 +17,44 @@ class First extends StatefulWidget {
 }
 
 class _FirstState extends State<First> {
-
-  var myName="My";
-  TextEditingController _myController = TextEditingController();
+  var Url='https://jsonplaceholder.typicode.com/photos';
+  var data;
+  // var myName="My";
+  // TextEditingController _myController = TextEditingController();
   
+  @override
+  void initState(){
+    super.initState();
+    fetchData();
+  }
+
+  fetchData()async{
+    var res = await http.get(Uri.parse(Url));
+    data=jsonDecode(res.body);
+    setState(() {
+      
+    });
+  }
+
+@override
+  void dispose(){
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: SingleChildScrollView(
-          child: NameCardWidget(myName: myName, myController: _myController),
-        ),
-          
-      ),
+     
       
         
         floatingActionButton: FloatingActionButton(
           
           onPressed: (){
-            myName=_myController.text;
-            setState(() {
+            // myName=_myController.text;
+            // setState(() {
               
-            });
+            // });
           },
 
 
@@ -47,6 +67,21 @@ class _FirstState extends State<First> {
         
         floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     
+        
+
+      body: data!=null?ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (context,index){
+          return ListTile(
+            title: Text(data[index]["title"]),
+            subtitle: Text("Id: ${data[index]['id']}"),
+            leading: Image.network(data[index]['url'])
+          );
+        }
+      )
+      :Center(
+        child: CircularProgressIndicator()
+      ),
 
 
       drawer: Mydrawer(),
